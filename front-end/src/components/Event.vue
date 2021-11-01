64ae79e78d02aab0b682377af3a5beb31bf7fc68
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2021-11-01 19:20:57 +0100

    started implementing sync between reminder date and days

--- front-end/src/components/Event.vue
+++ front-end/src/components/Event.vue
@@ -34,6 +34,23 @@
                       class="form-control"
                       type="date"
                       v-model="reminderDate"
+                      required
+                    />
+                  </div>
+                  <div class="col-2">
+                    <input
+                      class="form-control text-center"
+                      type="time"
+                      v-model="reminderTime"
+                      required
+                    />
+                  </div>
+                  <div class="col-5">
+                    <input
+                      class="form-control text-center"
+                      type="number"
+                      v-model="reminderDays"
+                      required
                     />
                   </div>
                 </div>
@@ -48,8 +65,12 @@
                   </div>
                 </div>
                 <div class="row mt-2">
-                  <div class="col-12 align-self-end">
-                    <button type="submit" class="btn btn-primary">
+                  <div class="col-12 btn-group d-flex" role="group">
+                    <button
+                      type="submit"
+                      class="btn btn-primary"
+                      @click="handleSubmit"
+                    >
                       Submit
                     </button>
                   </div>
@@ -71,20 +92,20 @@ export default defineComponent({
 
   data() {
     return {
-      eventDate: new Date(2021, 11, 17, 18, 0, 0),
+      eventDate: new Date(2021, 11, 17, 18, 0, 0, 0),
+      now: new Date(),
       reminderDate: "",
-      reminderTime: new Date(),
+      reminderTime: "13:00",
       reminderDays: 5,
       reminderEmail: "",
     };
   },
 
   methods: {
-    initialize() {
-      const now = new Date();
-      const reminderDate = new Date(
-        now.setDate(now.getDate() + this.reminderDays)
-      );
+    calculateReminderDate() {
+      const reminderDate = new Date(this.now.getTime());
+
+      reminderDate.setDate(reminderDate.getDate() + this.reminderDays);
 
       // we need to build a date string with format YYYY-MM-DD in order for binding to input with type="date" to work
       // TODO: consider moving this to separate method, or use external package
@@ -98,16 +119,53 @@ export default defineComponent({
         reminderDateDay < 10
           ? `0${reminderDateDay}`
           : reminderDateDay.toString();
-      this.reminderDate = `${reminderDate.getFullYear()}-${reminderDateMonthString}-${reminderDateDayString}`;
 
-      this.reminderTime = new Date(reminderDate.getTime());
-      this.reminderTime.setHours(13);
-      this.reminderTime.setMinutes(0);
+      return `${reminderDate.getFullYear()}-${reminderDateMonthString}-${reminderDateDayString}`;
+    },
+
+    calculateReminderDays() {
+      const fullYear = parseInt(this.reminderDate.substring(0, 4));
+      const month = parseInt(this.reminderDate.substring(5, 7)) - 1;
+      const day = parseInt(this.reminderDate.substring(8));
+      const reminderDate = new Date(fullYear, month, day, 0, 0, 0, 0);
+
+      const reminderTime = Math.abs(
+        reminderDate.getTime() - this.now.getTime()
+      );
+      const reminderDays = Math.ceil(reminderTime / (1000 * 60 * 60 * 24));
+
+      return reminderDays;
+    },
+
+    handleSubmit() {
+      console.log(this.reminderTime);
     },
   },
 
   mounted() {
-    this.initialize();
+    this.reminderDate = this.calculateReminderDate();
+  },
+
+  watch: {
+    reminderDate() {
+      const reminderDays = this.calculateReminderDays();
+
+      if (this.reminderDays === reminderDays) {
+        return;
+      }
+
+      this.reminderDays = reminderDays;
+    },
+
+    reminderDays() {
+      const reminderDate = this.calculateReminderDate();
+
+      if (this.reminderDate === reminderDate) {
+        return;
+      }
+
+      this.reminderDate = reminderDate;
+    },
   },
 });
 </script>
