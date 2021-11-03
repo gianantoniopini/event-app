ff090376ba3de18c400288613baf8cff3ffd0a38
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2021-11-03 13:48:34 +0100

    responsive layout changes

--- front-end/src/components/EventReminder.vue
+++ front-end/src/components/EventReminder.vue
@@ -32,7 +32,7 @@
                   <div class="col-12 fw-bold">Set a check back reminder</div>
                 </div>
                 <div class="row">
-                  <div class="col-xl-5 col-sm-8 mt-2">
+                  <div class="col-sm-8 col-lg-4 mt-2">
                     <input
                       class="form-control"
                       type="date"
@@ -45,7 +45,7 @@
                       required
                     />
                   </div>
-                  <div class="col-xl-2 col-sm-4 mt-2">
+                  <div class="col-sm-4 col-lg-3 mt-2">
                     <input
                       class="form-control text-center"
                       type="time"
@@ -58,7 +58,7 @@
                       required
                     />
                   </div>
-                  <div class="col-xl-5 col-sm-12 mt-2">
+                  <div class="col-sm-12 col-lg-5 mt-2">
                     <div class="row">
                       <div class="col-2 align-self-center text-end">in</div>
                       <div class="col-4">
