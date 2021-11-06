211d9c906cde3578341454b6ae418acc459e32ce
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2021-11-06 15:31:03 +0100

    changes as per Lighthouse suggestions: added compression

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const zlib = require("zlib");
+
 module.exports = {
   devServer: {
     port: process.env.VUE_APP_DEV_SERVER_PORT,
@@ -7,6 +10,27 @@ module.exports = {
     index: {
       entry: "src/main.ts",
       title: "Event App",
+    },
+  },
+  pluginOptions: {
+    compression: {
+      brotli: {
+        filename: "[file].br[query]",
+        algorithm: "brotliCompress",
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: "[file].gz[query]",
+        algorithm: "gzip",
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      },
     },
   },
 };
